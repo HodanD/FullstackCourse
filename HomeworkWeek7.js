3d220b2a213088e56bfcb0332ea2e506faa16228
“HodanD <hodan.d03@gmail.com>
2023-01-10 18:54:29 +0000

    Javascript Practice

---
+++ HomeworkWeek7.js
@@ -0,0 +1,41 @@
+//Using variables create a variable for a number, a string. Then print each of these variables
+var name = "Hodan"
+console.log (name)
+
+var age = 26
+console.log (age)
+
+var ethnicity = "Somali"
+console.log (ethnicity)
+
+//Using string concatenation add to different strings together
+console.log ("Hello my name is " + name + " , I am " + age + " years old " + "and I am " + ethnicity + ".") 
+
+
+//create an array with 15 items inside
+
+var array = ["Tiger", "Lion", "Gorrilla", "Elephant", "Lemur", "Giraffe", "Zebra", "Hippo", "Rhion", "Snake", "Leopard" , "Sloth", "Chimpanzee", "Jaguar", "Bear"]
+
+console.log(array)
+
+//remove the last item in the array
+array.pop()
+console.log (array)
+
+//Add an item to the end of the list.
+array.push ("Bat")
+console.log (array)
+
+//Changing an item name 
+array [13] = "Lizard"
+console.log (array)
+
+//Find the position of the first item, middle item and last item of the list and print them off separately:
+//First Item 
+console.log(array[0])
+//Middle Item
+console.log (array[7])
+//Last Item 
+console.log(array[14])
+
+
